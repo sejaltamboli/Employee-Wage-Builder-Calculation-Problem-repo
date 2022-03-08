398075679d30160ea1fd304dca5239bb48cb3857
sejaltamboli <sejaltamboli0602@gmail.com>
2022-03-07 20:58:06 -0800

    MODIFIED: Employee Part Time Wage Calculation

--- src/com/infogalaxy/empwagebuilder/EmployeeWageBuilder.java
+++ src/com/infogalaxy/empwagebuilder/EmployeeWageBuilder.java
@@ -8,7 +8,8 @@ public class EmployeeWageBuilder {
         System.out.println("Welcome To Employee Wage Made By Sejal Tamboli");
         //u-2 Calculate Daily Employee Wage
         final int WAGE_PER_HR = 20;
-        final int FULL_DAY_HR = 8;
+        final int PART_TIME = 4;
+        final int FULL_TIME = 8;
         int total_wage = 0;
         //UC-1 Employee Attendance
         Random random=new Random();
@@ -16,9 +17,17 @@ public class EmployeeWageBuilder {
             System.out.println(empAttendance);
         if(empAttendance == 0) {
             System.out.println("Employee is Absent");
-        }else{
+        }else {
             System.out.println("Employee is Present");
-            total_wage = WAGE_PER_HR * FULL_DAY_HR;
+            //UC-3 Add Part Time Employee and Wage
+            int emp_Type = random.nextInt() % 2;
+            if (emp_Type == 0) {
+                System.out.println("Employee is Part Time");
+                total_wage = WAGE_PER_HR * PART_TIME;
+            } else {
+                System.out.println("Employee is Full Time");
+                total_wage = WAGE_PER_HR * FULL_TIME;
+            }
         }
         System.out.println("Total Wage = " + total_wage);
     }
